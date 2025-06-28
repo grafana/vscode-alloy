f7691ab33e36eca665b5d136e443db5f4d51ecae
Fabien CAYRE <fabiencayre81@gmail.com>
2025-06-28 18:35:59 +0200

    feat: format an unsaved document

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import { exec } from "child_process";
+import { promisify } from "util";
 import * as vscode from "vscode";
 
 export function activate(_: vscode.ExtensionContext) {
@@ -16,16 +17,49 @@ export function activate(_: vscode.ExtensionContext) {
     vscode.languages.registerDocumentFormattingEditProvider("grafana-alloy", {
       provideDocumentFormattingEdits(
         document: vscode.TextDocument
-      ): vscode.TextEdit[] {
-        exec("alloy fmt -w " + document.fileName, (error, stdout, stderr) => {
-          if (error) {
-            vscode.window.showErrorMessage(`Error: ${error.message}`);
-          } else if (stderr) {
-            vscode.window.showErrorMessage(`Error: ${stderr}`);
-          } else if (stdout) {
-            vscode.window.showInformationMessage(`Formatted: ${stdout}`);
-          }
-        });
+      ): vscode.ProviderResult<vscode.TextEdit[]> {
+        // If dirty, use stdin and stdout from alloy fmt.
+        if (document.isDirty) {
+          const execWithStdin = (cmd: string, stdin: string, callback: (error: Error | null, stdout: string, stderr: string) => void) => {
+            const process = exec(cmd, (error, stdout, stderr) => {
+              if (error) {
+                callback(error, "", stderr);
+              } else {
+                callback(null, stdout, stderr);
+              }
+            });
+            if (process.stdin) {
+              process.stdin.write(stdin);
+              process.stdin.end();
+            }
+          };
+          const promisifiedExec = promisify(execWithStdin);
+
+          return promisifiedExec("alloy fmt", document.getText())
+            .then((result) => {
+              return [
+                vscode.TextEdit.replace(
+                  new vscode.Range(0, 0, document.lineCount, 0),
+                  result
+                ),
+              ];
+            })
+            .catch((error) => {
+              vscode.window.showErrorMessage(`Error: ${error.message}`);
+              return [];
+            });
+        } else {
+          // Otherwise overwrite the file with alloy fmt.
+          exec("alloy fmt -w " + document.fileName, (error, stdout, stderr) => {
+            if (error) {
+              vscode.window.showErrorMessage(`Error: ${error.message}`);
+            } else if (stderr) {
+              vscode.window.showErrorMessage(`Error: ${stderr}`);
+            } else if (stdout) {
+              vscode.window.showInformationMessage(`Formatted: ${stdout}`);
+            }
+          });
+        }
 
         return [];
       },
@@ -33,4 +67,4 @@ export function activate(_: vscode.ExtensionContext) {
   });
 }
 
-export function deactivate() {}
+export function deactivate() { }
